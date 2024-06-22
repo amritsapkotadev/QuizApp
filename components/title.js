de779b399f944b1ff2f63105159615dc4f1f36ce
amritsapkotadev <amritsapkota.dev@gmail.com>
2024-06-22 23:33:20 +0545

    Title is perfect

--- components/title.js
+++ components/title.js
@@ -1,12 +1,22 @@
-import { View, Text } from 'react-native'
+import { View, Text ,StyleSheet} from 'react-native'
 import React from 'react'
 
 const Title = () => {
   return (
     <View>
-      <Text>QUIZZLER</Text>
+      <Text style ={styles.title}>Quizzler</Text>
     </View>
   )
 }
 
 export default Title
+const styles =StyleSheet.create({
+    title:{
+        fontSize: 30,
+        color: 'black',
+        fontWeight: 'bold',
+        textAlign: 'center',
+        justifyContent: 'center',
+        paddingVertical: 20,
+    }
+    })
